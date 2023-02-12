12686cc17c916d27c51874d5b011c091b95d22c5
TotiFlame <santifer4321@gmail.com>
2023-02-12 15:19:32 -0300

    Mobile responsive done

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const nav = document.querySelector('#mobile-nav');
   const button = document.querySelector('#menu-icon');
+  const prodli = document.querySelector('#prod-link');
+  const aboutli = document.querySelector('#about-link');
+  const contactli = document.querySelector('#contact-link');
+
 
   button.addEventListener('click', () => {
     if (nav.classList.contains('show')) {
@@ -8,5 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       nav.classList.add('show');
     }
+  });
+
+  prodli.addEventListener('click', () => {
+    nav.classList.remove('show');
+  });
+
+  aboutli.addEventListener('click', () => {
+    nav.classList.remove('show');
+  });
+
+  contactli.addEventListener('click', () => {
+    nav.classList.remove('show');
   });
 });
